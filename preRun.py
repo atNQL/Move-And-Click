cdf30546f8e58aded35dd19a1dcafa64cdf5cb34
_Atom <luatnguyen.ee@gmail.com>
2021-09-11 19:54:53 +0700

    Initial version - Run

---
+++ preRun.py
@@ -0,0 +1,24 @@
+import pygetwindow as gw
+
+print(gw.getAllTitles())
+print(gw.getWindowsWithTitle('MEmu'))
+
+# 1 SCREEN
+TOPLEFT_X = 80
+TOPLEFT_Y = 65
+MEMU_W = 844
+MEMU_H = 485
+
+# 2 SCREEN
+# TOPLEFT_X = 2086
+# TOPLEFT_Y = 25
+# MEMU_W = 1650
+# MEMU_H = 941
+
+memudWindow  = gw.getWindowsWithTitle('MEmu')[0]
+# print(memudWindow.width)
+# print(memudWindow.height)
+memudWindow.moveTo(TOPLEFT_X, TOPLEFT_Y)
+# memudWindow.resizeTo(MEMU_W, MEMU_H) # 2 SCREEN
+
+
---
+++ run.py
@@ -0,0 +1,155 @@
+import time
+import threading
+from pynput.keyboard import Listener, KeyCode
+from pynput.mouse import Button, Controller
+from datetime import datetime
+import pyautogui
+
+class btn:
+    def __init__(self, btn_position_x, btn_position_y):
+        self.x = btn_position_x
+        self.y = btn_position_y
+
+REFRESH_PAGE_TIME = 3
+
+# 1 screen
+refrestPageBtn = btn(1069,187)
+crownBtn = btn(156,483)
+refresNumberBtn = btn(960,315)
+bigBtn = btn(511,515)
+smallBtn = btn(850,515)
+moneyInput = btn(710,620)
+okBtn = btn(967,620)
+confirmBtn = btn(525,590)
+informBtn = btn(525,500)
+# 2 screen
+# refrestPageBtn = btn(4530,210)
+# crownBtn = btn(2725,800)
+# refresNumberBtn = btn(4315,480)
+# bigBtn = btn(3471,865)
+# smallBtn = btn(3900, 880)
+# moneyInput = btn(3790, 1070)
+# okBtn = btn(4308, 1070)
+# confirmBtn = btn(3471, 1010)
+# informBtn = btn(3471, 800)
+
+delay = 2
+button =  Button.left
+
+start_stop_key = KeyCode(char='s')
+exit_key = KeyCode(char='e')
+
+class ClickMouse(threading.Thread):
+    def __init__(self, delay, button):
+        super().__init__()
+        self.delay = delay
+        self.button = button
+        self.running = False
+        self.program_running = True
+        self.count = 1
+        
+
+    def start_clicking(self):
+        self.running = True
+        print("Start autoclick now\n")
+
+    def stop_clicking(self):
+        self.running = False
+        print("Stop autoclick now")
+
+    def exit(self):
+        self.stop_clicking()
+        self.program_running  = False
+
+    def refreshPage(self):
+        pyautogui.moveTo(refrestPageBtn.x,refrestPageBtn.y, duration = 1)
+        mouse.click(button)
+        time.sleep(0.5)
+        mouse.click(button)
+        time.sleep(2)
+        pyautogui.moveTo(crownBtn.x, crownBtn.y, duration = 1)
+        mouse.click(button)
+
+        current_time = time.time()
+        endtime = current_time + 3
+        while current_time < endtime:
+            pyautogui.scroll(-10000000)
+            current_time = time.time()
+        mouse.click(button)
+        time.sleep(2)
+        pyautogui.moveTo(bigBtn.x, bigBtn.y, duration = 1)
+        current_time = time.time()
+        endtime = current_time + 3
+        while current_time < endtime:
+            pyautogui.scroll(-10000000)
+            current_time = time.time()
+    
+    def click_n_delay(self,x,y, t_delay):
+        pyautogui.moveTo(x, y, duration = 1)
+        mouse.click(self.button)
+        time.sleep(t_delay)    
+
+    def click_procedure(self,typeBtn,value):
+        # move to BIG
+        self.click_n_delay(typeBtn.x,typeBtn.y,self.delay)
+        # move to INPUT - moneyInput
+        self.click_n_delay(moneyInput.x,moneyInput.y,0.5)
+        pyautogui.typewrite(value)
+        time.sleep(1)
+        # move to OK - okBtn
+        self.click_n_delay(okBtn.x,okBtn.y,1)
+        # move to CONFIRM - confirmBtn
+        self.click_n_delay(confirmBtn.x,confirmBtn.y,3)
+        # move to close information - informBtn
+        self.click_n_delay(informBtn.x,informBtn.y,self.delay)
+    
+    def run(self):
+        while self.program_running:
+            while self.running:
+                print("Time  ------- ", self.count)
+                time.sleep(1)
+                print("...3")
+                time.sleep(1)
+                print("...2")
+                time.sleep(1)
+                print("...1")
+                time.sleep(1)
+                print("...0")
+                print(time.strftime('%X %x %Z'))
+                if self.count % REFRESH_PAGE_TIME == 0:
+                    print("Refresh page !")
+                    self.refreshPage()
+                else:
+                    self.count = self.count + 1
+
+                # refresh NUMBER
+                self.click_n_delay(refresNumberBtn.x,refresNumberBtn.y,self.delay)
+                # CALL BIG
+                self.click_procedure(bigBtn, "16481")
+                # CALL SMALL
+                self.click_procedure(smallBtn, "18518")
+
+                # reset mouse position to prevent unknown action
+                pyautogui.moveTo(156, 900, duration = 1)
+                if self.count % REFRESH_PAGE_TIME == 1:
+                    time.sleep(246)
+                else:
+                    time.sleep(261)
+
+mouse = Controller()
+click_thread =  ClickMouse(delay,button)
+click_thread.start()
+
+def on_press(key):
+    if key == start_stop_key:
+        if click_thread.running:
+            click_thread.stop_clicking()
+        else:
+            click_thread.start_clicking()
+    elif key == exit_key:
+        click_thread.exit()
+        listener.stop()
+
+with Listener(on_press = on_press) as listener:
+    print("READY!")
+    listener.join()

f3b92b5cc5c48cc08047ce69e0a1d9a661076708
_Atom <luatnguyen.ee@gmail.com>
2021-09-11 22:53:42 +0700

    Resize memu to standard size

--- preRun.py
+++ preRun.py
@@ -19,6 +19,6 @@ memudWindow  = gw.getWindowsWithTitle('MEmu')[0]
 # print(memudWindow.width)
 # print(memudWindow.height)
 memudWindow.moveTo(TOPLEFT_X, TOPLEFT_Y)
-# memudWindow.resizeTo(MEMU_W, MEMU_H) # 2 SCREEN
+memudWindow.resizeTo(MEMU_W, MEMU_H)
 
 

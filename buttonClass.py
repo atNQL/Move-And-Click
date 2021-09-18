dd1e1c3e7cde6a0e75b2917dfbe9dbd6f1649fd5
_Atom <luatnguyen.ee@gmail.com>
2021-09-18 10:01:13 +0700

    No refreshpage, fix run SMALL firstly

--- buttonClass.py
+++ buttonClass.py
@@ -1,5 +1,4 @@
 from pynput.mouse import Button, Controller
-from datetime import datetime
 import pyautogui
 import random
 import winsound
@@ -20,7 +19,7 @@ class btn:
         self.message = message
         self.rgb = rgb_input
 class run:
-    def __init__(self, refreshPageBtn, crownBtn, refresNumberBtn, bigBtn, smallBtn, moneyInput, okBtn, confirmBtn,confirmBtn_buff, informBtn,menuBtn,recordBtn,rmBtn, confirmRm,cap):
+    def __init__(self, refreshPageBtn, crownBtn, refresNumberBtn, bigBtn, smallBtn, moneyInput, okBtn, confirmBtn,selfCheckBtn, informBtn,menuBtn,recordBtn,rmBtn, confirmRm,cap):
         self.refreshPageBtn = refreshPageBtn
         self.crownBtn = crownBtn
         self.refresNumberBtn = refresNumberBtn
@@ -30,7 +29,7 @@ class run:
         self.moneyInput = moneyInput
         self.okBtn = okBtn
         self.confirmBtn = confirmBtn
-        self.confirmBtn_buff = confirmBtn_buff
+        self.selfCheckBtn = selfCheckBtn
         self.informBtn = informBtn
         self.delay = delay
         self.cap = cap
@@ -41,11 +40,17 @@ class run:
         self.recordBtn = recordBtn
         self.rmBtn = rmBtn
         self.confirmRm = confirmRm
+        self.profit = 0
 
-    def setNewCap(self,newValue):
-        self.cap = newValue
-        self.bigVal = str(round(newValue*0.4709))
-        self.smallVal = str(round(newValue*0.5291))
+    def calProfitNewCap(self):
+        self.profit = self.profit + round(self.cap*0.000736) 
+        if self.profit >= 100:
+            self.cap = self.cap + 100
+            self.bigVal = str(round(self.cap*0.4709))
+            self.smallVal = str(round(self.cap*0.5291))
+            self.profit = self.profit - 100
+            print ("Next cap is ", self.cap)
+        
     
     def refreshPage(self):
         pyautogui.moveTo(self.refreshPageBtn.x,self.refreshPageBtn.y, duration = 1)
@@ -84,45 +89,48 @@ class run:
 
     def removeSubmitted(self):
         print ("Remove summit!")
-        pyautogui.moveTo(self.refreshPageBtn.x,self.refreshPageBtn.y, duration = 1)
-        mouse.click(button)
-        time.sleep(0.5)
-        mouse.click(button)
-        time.sleep(4)
-        self.click_n_delay(self.menuBtn,self.delay)
-        self.click_n_delay(self.recordBtn,self.delay)
-        time.sleep(7)
-        self.click_n_delay(self.rmBtn,self.delay)
-        self.click_n_delay(self.confirmRm,self.delay)
+        # pyautogui.moveTo(self.refreshPageBtn.x,self.refreshPageBtn.y, duration = 1)
+        # mouse.click(button)
+        # time.sleep(0.5)
+        # mouse.click(button)
+        # time.sleep(4)
+        # self.click_n_delay(self.menuBtn,self.delay)
+        # self.click_n_delay(self.recordBtn,self.delay)
+        # time.sleep(7)
+        # self.click_n_delay(self.rmBtn,self.delay)
+        # self.click_n_delay(self.confirmRm,self.delay)
 
     def interrupt(self, mess):
+        print("Interrupt at: ", mess)
         for i in range(10):
             winsound.Beep(2500,300)
             time.sleep(0.1)
-
-        print("Interrupt at: ", mess)
+        
         if mess == "CEP":
             while True:
-                winsound.Beep(2500,5000)
+                winsound.Beep(2500,3000)
                 time.sleep(1)
         if self.status == 1:
             self.removeSubmitted()
         while True:
-                winsound.Beep(2500,5000)
+                winsound.Beep(2500,3000)
                 time.sleep(1)
 
     def checkRGB(self,x, y, rgb_check, mess):
         rgb = PIL.ImageGrab.grab().load()[x,y]
         if rgb != rgb_check:
-            self.interrupt(mess, " RGB wrong!")
+            mess = mess + " RGB wrong!"
+            self.interrupt(mess)
 
     def click_procedure(self,typeBtn,value):
+        if typeBtn.message == "Big":
+            self.checkRGB(self.smallBtn.x,self.smallBtn.y,self.smallBtn.rgb,"Second check small")
+        elif typeBtn.message == "Small":
+            self.checkRGB(self.bigBtn.x,self.bigBtn.y,self.bigBtn.rgb,"Second check big")
+        
         # move to BIG/SMALL
         self.click_n_delay(typeBtn,self.delay)
-        if typeBtn.message == "Big":
-            self.checkRGB(typeBtn.x,typeBtn.y,(241,49,49), typeBtn.message)
-        elif typeBtn.message == "Small":
-            self.checkRGB(typeBtn.x,typeBtn.y,(241,49,49), typeBtn.message)
+        self.checkRGB(typeBtn.x,typeBtn.y,(241,49,49), typeBtn.message)
 
         # move to INPUT - moneyInput
         self.click_n_delay(self.moneyInput,self.delay)
@@ -133,24 +141,22 @@ class run:
         if rgb == self.moneyInput.rgb:
             self.interrupt(" Can not input money!")
         else:
-            print("Input",typeBtn.message, ":\tsuccessfully!")    
+            print("\tInput",typeBtn.message, ":\tsuccessfully!")    
         # move to OK - okBtn
         self.click_n_delay(self.okBtn,1)
         # move to CONFIRM - confirmBtn
         self.click_n_delay(self.confirmBtn,3)
         self.status = 1
-        # move to CONFIRM BUFF - confirmBtn
-        # self.click_n_delay(self.confirmBtn_buff.x,self.confirmBtn_buff.y,2)
         # move to close information - informBtn
         rgb = PIL.ImageGrab.grab().load()[self.informBtn.x,self.informBtn.y]
-        endtime = time.time() + 5
+        endtime = time.time() + 7
         while rgb != self.informBtn.rgb:
             print("Over 3s for confirmation form appear!!!")
             current_time = time.time()
             rgb = PIL.ImageGrab.grab().load()[self.informBtn.x,self.informBtn.y]
             if current_time > endtime:
                 self.interrupt("CEP")
-            time.sleep(0.5)
+            time.sleep(1)
 
         self.click_n_delay(self.informBtn,self.delay)
 
@@ -160,24 +166,34 @@ class run:
         #     print("\tRefresh page !")
         #     self.refreshPage()
 
+        rgb = PIL.ImageGrab.grab().load()[self.selfCheckBtn.x,self.selfCheckBtn.y]
+        if rgb == self.selfCheckBtn.rgb:
+            self.interrupt("Self check RGB wrong!")
+        self.checkRGB(self.crownBtn.x, self.crownBtn.y,self.crownBtn.rgb,"First check crown")
+        # Check big and small before run
+        self.checkRGB(self.bigBtn.x,self.bigBtn.y,self.bigBtn.rgb,"First check big")
+        self.checkRGB(self.smallBtn.x,self.smallBtn.y,self.smallBtn.rgb,"First check small")
+        
+
         # refresh NUMBER
         self.click_n_delay(self.refresNumberBtn,2)
-    
-        rgb = PIL.ImageGrab.grab().load()[self.bigBtn.x,self.bigBtn.y]
-        self.checkRGB(self.bigBtn.x,self.bigBtn.y,self.bigBtn.rgb,"First check big")
-        rgb = PIL.ImageGrab.grab().load()[self.smallBtn.x,self.smallBtn.y]
-        self.checkRGB(self.smallBtn.x,self.smallBtn.y,self.smallBtn.rgb,"First check small")
 
-        n = random.randint(0,1)
-        if n == 0 and self.status == 0:
-            # CALL BIG - SMALL
-            self.click_procedure(self.bigBtn, self.bigVal)
-            self.click_procedure(self.smallBtn, self.smallVal)
-            self.status = 0
-        elif n == 1 and self.status == 0:
+        if self.status == 0:
             # CALL SMALL - BIG
             self.click_procedure(self.smallBtn, self.smallVal)
             self.click_procedure(self.bigBtn, self.bigVal)
             self.status = 0
+
+        # n = random.randint(0,1)
+        # if n == 0 and self.status == 0:
+        #     # CALL BIG - SMALL
+        #     self.click_procedure(self.smallBtn, self.smallVal)
+        #     self.click_procedure(self.bigBtn, self.bigVal)
+        #     self.status = 0
+        # elif n == 1 and self.status == 0:
+        #     # CALL SMALL - BIG
+        #     self.click_procedure(self.smallBtn, self.smallVal)
+        #     self.click_procedure(self.bigBtn, self.bigVal)
+        #     self.status = 0
 
 mouse = Controller()
